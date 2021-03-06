461f9e1aabcdff0c1788310b050c8a4a8e090050
Hirgy <75968043+hirgy@users.noreply.github.com>
2021-03-06 17:31:01 +0700

    Update latihan1a.php

--- Tugas1/latihan1a.php
+++ Tugas1/latihan1a.php
@@ -7,7 +7,7 @@ Shift : Rabu pukul 09:00 - 10:00 WIB
 <?php 
 for ($i = 1; $i <= 3; $i++) {
     for ($j = 1; $j <=3 ; $j++){
-        echo "ini perulangan ke ($i,$j)<br>"
+        echo "ini perulangan ke ($i,$j)<br>";
     }
 }
 
