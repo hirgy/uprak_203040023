ebce8254d99ee7b1ea064147ad46f7e9ba757d64
hirgy <hirgy9@gmail.com>
2021-03-11 15:08:31 +0700

    tugas 2 uprak pemrograman web

---
+++ latihan2a.php
@@ -0,0 +1,49 @@
+<?php
+
+// Nama :Muhammad Rhyamizard Hirgy
+// NRP : 203040023
+// Shift : Rabu pukul 09:00-10:00 WIB
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Latihan2a_203040083</title>
+    <style>
+        .tulisan {
+            font-size: 28px;
+            font-family: arial;
+            color: #8c782d;
+            font-style: italic;
+            font-weight: bolder;
+            padding-left: 20px;
+        }
+
+        .pembungkus {
+            border: 1px solid black;
+            box-shadow: 2px 2px 3px 3px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+
+    function gantiStyle($tulisan, $style1, $style2)
+    {
+        echo "<div class = '$style1'> 
+            <h3 class =  '$style2'>  $tulisan </h3>
+        </div>";
+    }
+    ?>
+
+    <?php
+    echo gantiStyle("Selamat datang di praktikum PW", "pembungkus", "tulisan");
+    ?>
+</body>
+
+
+</html>
---
+++ latihan2b.php
@@ -0,0 +1,48 @@
+<?php
+ 
+// Nama :Muhammad Rhyamizard Hirgy
+// NRP : 203040023
+// Shift : Rabu pukul 09:00-10:00 WIB
+
+?>
+
+<?php
+$jawabanIsset = "Isset adalah = Digunakan untuk memeriksa apakah suatu variabel sudah diatur atau belum. <br> <br>";
+$jawabanEmpty = "Empty adalah = Digunakan untuk membuat sebuah validasi dengan php untuk melakukan pemeriksaan terhadap variabel-variabel yang berada di luar php.";
+
+$GLOBALS["isset"] = $jawabanIsset;
+$GLOBALS["empty"] = $jawabanEmpty;
+
+
+function soal($style)
+{
+    echo "<div class = $style>
+    <h1>" . $GLOBALS['isset'] . "</h1>
+    <h1>" . $GLOBALS['empty'] . "</h1>
+    </div>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Latihan2b_203040083</title>
+    <style>
+        .center {
+            border: 1px solid black;
+            padding-left: 10px;
+            font-size: 8px;
+            color: goldenrod;
+            font-family: monospace;
+            background-color: #222222;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    echo soal("center");
+    ?>
+</body>
+
+</html>
